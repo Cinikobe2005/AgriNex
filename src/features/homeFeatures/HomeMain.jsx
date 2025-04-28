65a37d93f6663566d737df706ac502e7fc9e3037
Cinikobe2005 <adedayoabdulqadri@gmail.com>
2025-04-28 04:06:37 -0700

    moved img to public folder

--- src/features/homeFeatures/HomeMain.jsx
+++ src/features/homeFeatures/HomeMain.jsx
@@ -3,7 +3,7 @@ import Button from "../../components/Button";
 
 const HomeMain = () => {
   return (
-    <main className="flex flex-col justify-center md:items-start items-center md:text-left text-center gap-5 h-screen bg-[url(src/assets/tractor.png)] bg-cover backdrop-brightness-100 text-white py-4 px-6 md:px-10 lg:px-20">
+    <main className="flex flex-col justify-center md:items-start items-center md:text-left text-center gap-5 h-screen bg-[url(public/tractor.png)] bg-cover backdrop-brightness-100 text-white py-4 px-6 md:px-10 lg:px-20">
       <div className="w-11/12 mx-auto container">
 
       <h1 className="w-full text-[32px] md:w-[80%] md:text-[48px] lg:w-[70%] lg:text-[56px] font-bold">
