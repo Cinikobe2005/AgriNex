f40f88d72e551773b03019883482cbc29c98b3f5
Cinikobe2005 <adedayoabdulqadri@gmail.com>
2025-05-12 05:55:25 -0700

    update about us page and mad some changes

--- src/features/contactisFeatures/ContactusMain.jsx
+++ src/features/contactisFeatures/ContactusMain.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ContactusMain = () => {
   return (
-    <main className=" bg-[url(/ContactUsImg.png)] h-[636px] bg-cover backdrop-brightness-100 text-white py-4 px-3 md:px-[90px] lg:px-[100px]">
-      <div className="w-11/12 mx-auto container flex flex-col md:justify-end md:items-baseline justify-center items-center md:text-left text-center gap-5 h-[636px] pb-20">
-        <h1 className="w-full text-[25px] md:w-[80%] md:text-[48px] lg:w-[70%] lg:text-[56px] font-bold">
+    <main className="bg-[url(/holdingPhone.png)] h-[636px] bg-cover backdrop-brightness-100 text-white py-4 px-3 md:px-[90px] lg:px-[100px]">
+      <div className="w-11/12 mx-auto container flex flex-col justify-center items-center text-center md:text-left gap-5 h-[636px]">
+        <h1 className="w-full text-[25px] md:text-[48px] lg:text-[56px] font-bold">
           Contact Us
         </h1>
-        <p className="md:text-[32px] text-[20px]">
-          We’d love to hear from you! Whether you have questions about our 
-          services, need support, or just want to share your feedback — we’re 
+        <p className="text-[20px] md:text-[32px]">
+          We’d love to hear from you! Whether you have questions about our
+          services, need support, or just want to share your feedback — we’re
           here to help.
         </p>
       </div>
